699fa81f9da5cbb49aa5b4650697e7f72c477899
dea-alihajdari <116096272+dea-alihajdari@users.noreply.github.com>
2025-01-09 19:14:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-console.log('Goodbye!')
+console.log('Goodbye!');
+
+
+var _test = 5;
+var Test = 4;
+var $test = 4;
+// var 0test=4;
+var t0est = 4;
+var value1 = 4;
+var TEST = 6;
+var test = 4;
+
+console.log(TEST)
+console.log(test)
+
+var a = 50 + 50;
+console.log(a)
+
+var b = 20;
+var c = 30;
+var d = b + c;
+
+console.log(d)
+
+var e = 40;
+var f = 10;
+var g = e - f;
+
+console.log(g)
+
+var h = 100;
+var i = 5;
+var j = h / i;
+
+console.log(j)
+
+var k = 10;
+var l = 4;
+var m = k % l;
+console.log(m)
+
+var n = 99;
+n = n + 1;
+console.log(n)
+
+var count = 99;
+count++;
+console.log(count)
+
+var o = 101;
+o--;
+console.log(o)
