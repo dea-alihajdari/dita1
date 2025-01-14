e00cd7999360259117926effb45178f822bc9d64
dea-alihajdari <116096272+dea-alihajdari@users.noreply.github.com>
2025-01-14 19:13:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,108 @@
-console.log('Goodbye!');
+// console.log('Goodbye!');
 
 
-var _test = 5;
-var Test = 4;
-var $test = 4;
-// var 0test=4;
-var t0est = 4;
-var value1 = 4;
-var TEST = 6;
-var test = 4;
+// var _test = 5;
+// var Test = 4;
+// var $test = 4;
+// // var 0test=4;
+// var t0est = 4;
+// var value1 = 4;
+// var TEST = 6;
+// var test = 4;
 
-console.log(TEST)
-console.log(test)
+// console.log(TEST)
+// console.log(test)
 
-var a = 50 + 50;
-console.log(a)
+// var a = 50 + 50;
+// console.log(a)
 
-var b = 20;
-var c = 30;
-var d = b + c;
+// var b = 20;
+// var c = 30;
+// var d = b + c;
 
-console.log(d)
+// console.log(d)
 
-var e = 40;
-var f = 10;
-var g = e - f;
+// var e = 40;
+// var f = 10;
+// var g = e - f;
 
-console.log(g)
+// console.log(g)
 
-var h = 100;
-var i = 5;
-var j = h / i;
+// var h = 100;
+// var i = 5;
+// var j = h / i;
 
-console.log(j)
+// console.log(j)
 
-var k = 10;
-var l = 4;
-var m = k % l;
-console.log(m)
+// var k = 10;
+// var l = 4;
+// var m = k % l;
+// console.log(m)
 
-var n = 99;
-n = n + 1;
-console.log(n)
+// var n = 99;
+// n = n + 1;
+// console.log(n)
 
-var count = 99;
-count++;
-console.log(count)
+// var count = 99;
+// count++;
+// console.log(count)
 
-var o = 101;
-o--;
-console.log(o)
+// var o = 101;
+// o--;
+// console.log(o)
+
+//var r = 20;
+//r+=5;
+
+//console.log(r)
+
+var x = 35;
+x-=5;
+
+console.log(x)
+
+var y = 7;
+y*=10;
+
+console.log(y)
+
+var z = 20;
+z/=5;
+
+console.log(z)
+
+var t = 10;
+t%=4;
+
+console.log(t)
+
+var a = 5;
+var b = 5;
+
+console.log( a==b )
+
+var c = 5;
+var d = 6;
+
+console.log( c==d )
+
+
+var g = 5;
+var k = 8;
+
+console.log( g != k)
+
+var num1 = 100;
+var num2 = 10;
+
+console.log( num1 < num2)
+
+var num3 = 100;
+var num4 = 10;
+
+console.log( num3 > num4)
+
+var num5 = 10;
+var num6 = "10";
+
+console.log( num5===num6)
